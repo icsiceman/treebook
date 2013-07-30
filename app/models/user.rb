e741d0e5420cab43d6bb206ed25ea254baf91bf7
Eugene Lai <eugenejlai@gmail.com>
2013-07-30 00:30:33 -0700

    Still not working correctly, some errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   attr_accessible :title, :body, :first_name, :last_name, :email, :password, :password_confirmation, :profile_name
+
+  validates :first_name, presence: true
+  
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                           format: {
+                             with: /a-z/,
+                             message: 'Must be formated correctly'
+                           }
 
   has_many :statuses
   
